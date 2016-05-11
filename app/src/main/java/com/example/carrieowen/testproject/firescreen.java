7a767c3e68a6bd4684c7dac10e0399b57c624852
carrie.owen <carrie.owen@stu.henderson.kyschools.us>
2016-05-11 14:15:54 -0500

    sfhsj

--- app/src/main/java/com/example/carrieowen/testproject/firescreen.java
+++ app/src/main/java/com/example/carrieowen/testproject/firescreen.java
@@ -2,17 +2,12 @@ package com.example.carrieowen.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class firescreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_firescreen);
     }
 }
