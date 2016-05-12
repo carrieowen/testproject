3199cbedcf6535584ecdddde6a13459156791229
zebadiah.gentry <zebadiah.gentry@stu.henderson.kyschools.us>
2016-05-12 13:33:41 -0500

    Made first button work... again...

--- app/src/main/java/com/example/carrieowen/testproject/MainActivity.java
+++ app/src/main/java/com/example/carrieowen/testproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.carrieowen.testproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-
-
-
+    public void sendMessage (View view){
+        Intent changeScreen = new Intent(this, firescreen.class);
+        startActivity(changeScreen);
     }
 }
+
