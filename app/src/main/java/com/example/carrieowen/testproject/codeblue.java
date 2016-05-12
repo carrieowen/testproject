caf9c68b55f84b689b144ac8e251b341a5d4a575
carrie.owen <carrie.owen@stu.henderson.kyschools.us>
2016-05-12 13:45:06 -0500

    max and carrie work

---
+++ app/src/main/java/com/example/carrieowen/testproject/codeblue.java
@@ -0,0 +1,13 @@
+package com.example.carrieowen.testproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class codeblue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codeblue);
+    }
+}
