61a0f2239092dae6c8fc2c6ab80e60444645fff8
zebadiah.gentry <zebadiah.gentry@stu.henderson.kyschools.us>
2016-05-18 13:01:25 -0500

    DONE

--- app/src/main/java/com/example/carrieowen/testproject/codeblue.java
+++ app/src/main/java/com/example/carrieowen/testproject/codeblue.java
@@ -1,7 +1,9 @@
 package com.example.carrieowen.testproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class codeblue extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class codeblue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codeblue);
+    }
+    public void Main (View view){
+        Intent changeScreenMain = new Intent(this, MainActivity.class);
+        startActivity(changeScreenMain);
     }
 }
--- app/src/main/java/com/example/carrieowen/testproject/tornado.java
+++ app/src/main/java/com/example/carrieowen/testproject/tornado.java
@@ -1,7 +1,9 @@
 package com.example.carrieowen.testproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class tornado extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class tornado extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tornado);
+    }
+    public void Main (View view){
+        Intent changeScreenMain = new Intent(this, MainActivity.class);
+        startActivity(changeScreenMain);
     }
 }
